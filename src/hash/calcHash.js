bbbb04925b84769e45ff3fe6920d511c631829c4
denismezhenin <103366013+denismezhenin@users.noreply.github.com>
2023-06-18 21:36:07 +0200

    feat: hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,16 @@
+import fs from "fs";
+import path from "path";
+import { getDirPath } from "../utils/func-helper.js";
+import { createHash } from "crypto";
+
+const __dirname = getDirPath(import.meta.url);
+const file = path.join(__dirname, "files", "fileToCalculateHashFor.txt");
+
 const calculateHash = async () => {
-    // Write your code here 
+  const hash = createHash("sha256");
+  hash.update(file);
+  const hex = hash.digest("hex");
+  console.log(hex);
 };
 
 await calculateHash();

f49cfdf4556b6e958d50209bc7ca431db2139e9d
denismezhenin <103366013+denismezhenin@users.noreply.github.com>
2023-06-18 18:46:30 +0200

    fs: rename

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,4 +1,20 @@
+import path, { dirname } from "path";
+import fs from "fs";
+import { getDirPath } from "../utils/func-helper.js";
+
+const __dirname = (getDirPath(import.meta.url));
+const filesPath = path.join(__dirname, "files", "wrongFilename.txt");
+const filesCopyPath = path.join(__dirname, "files", "properFilename.md");
+
+if (!fs.existsSync(filesPath) && fs.existsSync(filesCopyPath)) {
+    throw new Error("FS operation failed");
+}
+
+
 const rename = async () => {
+    fs.rename(filesPath, filesCopyPath, err => {
+        if (err)  throw new Error("FS operation failed");
+    })
     // Write your code here 
 };
 

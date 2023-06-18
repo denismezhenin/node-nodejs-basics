f4e2aef5c14e30312bcd9b89b05346b4ac8eea24
denismezhenin <103366013+denismezhenin@users.noreply.github.com>
2023-06-18 19:39:25 +0200

    feat: fs:read

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,4 +1,16 @@
+import path, { dirname } from "path";
+import fs from "fs";
+import { getDirPath } from "../utils/func-helper.js";
+
+const __dirname = (getDirPath(import.meta.url));
+const filesToReadPath = path.join(__dirname, "files", "fileToRead.txt");
+
 const read = async () => {
+
+    fs.readFile(filesToReadPath, 'utf8', (err, data) => {
+        if (err)  throw new Error("FS operation failed");
+        console.log(data)
+    })
     // Write your code here 
 };
 

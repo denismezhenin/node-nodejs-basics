746f5ad8e5ccf41d9a0162b5fdfeebedd1812c5d
denismezhenin <103366013+denismezhenin@users.noreply.github.com>
2023-06-19 10:48:58 +0200

    feat: worker

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,16 +1,15 @@
 import path from "path";
 import { getDirPath } from "../utils/func-helper.js";
-import { spawn } from 'child_process';
+import { spawn } from "child_process";
 
 const __dirname = getDirPath(import.meta.url);
 const fileToScriptPath = path.join(__dirname, "files", "script.js");
 
 const spawnChildProcess = async (args) => {
-    const childProcess = spawn("node", [fileToScriptPath, ...args], {
-        stdio:[0, 1, 'ipc']
-    })
-
+  const childProcess = spawn("node", [fileToScriptPath, ...args], {
+    stdio: [0, 1, "ipc"],
+  });
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess([1,2,3]);
+spawnChildProcess([1, 2, 3]);
--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,37 @@
+import { Worker } from "worker_threads";
+import path from "path";
+import { getDirPath } from "../utils/func-helper.js";
+import { cpus } from "os";
+
+const __dirname = getDirPath(import.meta.url);
+const fileWorkerPath = path.join(__dirname, "worker.js");
+
+const transformArr = (arr) => {
+  return arr.map((el) => {
+    return (el =
+      el.status === "fulfilled"
+        ? { status: "resolved", data: el.value }
+        : { status: "error", data: null });
+  });
+};
+
 const performCalculations = async () => {
-    // Write your code here
+  // Write your code here
+  const promisesArray = [];
+  const numberOfCpus = cpus().length;
+
+  for (let i = 0; i < numberOfCpus; i += 1) {
+    const num = 10 + i;
+    const promise = new Promise((resolve, reject) => {
+      const worker = new Worker(fileWorkerPath, { workerData: num });
+      worker.on("message", resolve);
+      worker.on("error", reject);
+    });
+    promisesArray.push(promise);
+  }
+  const result = Promise.allSettled(promisesArray)
+    .then((result) => transformArr(result))
+    .then((arr) => console.log(arr));
 };
 
 await performCalculations();
